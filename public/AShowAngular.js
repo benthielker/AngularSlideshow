13c18f8dba5d53219c3d59bd266c940aea9fd3dd
Ben <benthielker@gmail.com>
2014-08-04 13:52:46 -0600

    Add improved responsiveness

--- public/AShowAngular.js
+++ public/AShowAngular.js
@@ -5,17 +5,17 @@
 // Use log function to issue additional console logs.
 //
 var verbose = false,
-	log = (function(msg) {
-		if (verbose) console.log(msg);
-	});
+   log = function(msg) {
+      if (verbose) console.log(msg);
+   };
 
 //
 // App module
 //
 var app = angular.module("AShow", [
-	"AShow.controllers",
-	"AShow.directives",
-	"AShow.services"
+   "AShow.controllers",
+   "AShow.directives",
+   "AShow.services"
 ]);
 
 //
@@ -24,11 +24,11 @@ var app = angular.module("AShow", [
 angular.module("AShow.controllers", [
 ]).
 controller("slideshowCtrl", ["$scope", "Slides",
-	function($scope, Slides) {
-		//
-		// All logic currently required is handled by directives and services.
-		//
-	}
+   function($scope, Slides) {
+      //
+      // All logic currently required is handled by directives and services.
+      //
+   }
 ]);
 
 //
@@ -37,282 +37,282 @@ controller("slideshowCtrl", ["$scope", "Slides",
 angular.module("AShow.directives", [
 ]).
 directive("aSlideshow", ['$compile', 'Slides', function($compile, Slides) {
-	return {
-		link : (function(scope, element) {
-			log("aSlideshow directive called for element.id '"+element.attr('id')+"'");
-			//
-			// Create AShow instance for element and register with scope.
-			// (Will be configured by element attributes.)
-			//
-			var thisShow = AShow.MakeShowWithElement(element);
-			// lookup slides for this show
-			//thisShow.slides = Slides.forShow(thisShow.slideshow_id); // if running as local file
-			thisShow.slides = Slides.query({showId:thisShow.slideshow_id}); // for json or api
-			if (typeof(scope.shows)=='undefined') scope.shows = {};
-			scope.shows[thisShow.slideshow_id] = thisShow;
-			//
-			// Populate element with .slides div which will hold slide content divs.
-			//
-			var slidesRepoHtml = "<div class='AShowSlides'>"+
-					"<div class='AShowSlide' ng-repeat='slide in shows."+thisShow.slideshow_id+".slides' a-slide></div>"+
-				"</div>";
-			var slidesRepo = $compile(slidesRepoHtml)(scope);
-			element.append(slidesRepo);
-		})
-	};
+   return {
+      link : function(scope, element) {
+         log("aSlideshow directive called for element.id '"+element.attr('id')+"'");
+         //
+         // Create AShow instance for element and register with scope.
+         // (Will be configured by element attributes.)
+         //
+         var thisShow = AShow.MakeShowWithElement(element);
+         // lookup slides for this show
+         //thisShow.slides = Slides.forShow(thisShow.slideshow_id); // if running as local file
+         thisShow.slides = Slides.query({showId:thisShow.slideshow_id}); // for json or api
+         if (typeof(scope.shows)=='undefined') scope.shows = {};
+         scope.shows[thisShow.slideshow_id] = thisShow;
+         //
+         // Populate element with .slides div which will hold slide content divs.
+         //
+         var slidesRepoHtml = "<div class='AShowSlides'>"+
+               "<div class='AShowSlide' ng-repeat='slide in shows."+thisShow.slideshow_id+".slides' a-slide></div>"+
+            "</div>";
+         var slidesRepo = $compile(slidesRepoHtml)(scope);
+         element.append(slidesRepo);
+      }
+   };
 }]).
 directive("aSlide", function() {
-	return {
-		link : (function(scope, element) {
-			log("aSlide directive called.");
-			log(". element.parent.AShow:"+element.parents('.AShow')[0].AShow);
-			//
-			// Create AShowSlide instance for this slide content element.
-			// Link to AShow instance assigned to the parent element.
-			//
-			var slide = scope.slide,
-				show = element.parents('.AShow')[0].AShow;
-			if (slide && show)
-			{	
-				var showId = show.slideshow_id;
-				log(". slide:"+slide+", show:"+show+" showId:"+showId);
-				
-				// populate element with slide content.
-				element.html(slide.content);
-				
-				// create AShowSlide instance for element.
-				var instance = AShowSlide.MakeSlideWithElementForShow(element, show, {
-					'index' : slide.index
-				});
-				
-				// Check if this is last slide to load.
-				if (scope.$last) {
-					// Display show navigation controls.
-					show.createControls();
-					// Call show's advance method to display first slide.
-					show.advance();
-				}
-			}
-			else
-			{	
-				if (!slide) throw new Error("aSlide directive ERROR: slide object not found in scope.");
-				if (!show) throw new Error("aSlide directive ERROR: show object '"+showId+"' not found in scope.");
-			}
-		})
-	};
+   return {
+      link : function(scope, element) {
+         log("aSlide directive called.");
+         log(". element.parent.AShow:"+element.parents('.AShow')[0].AShow);
+         //
+         // Create AShowSlide instance for this slide content element.
+         // Link to AShow instance assigned to the parent element.
+         //
+         var slide = scope.slide,
+            show = element.parents('.AShow')[0].AShow;
+         if (slide && show)
+         {   
+            var showId = show.slideshow_id;
+            log(". slide:"+slide+", show:"+show+" showId:"+showId);
+            
+            // populate element with slide content.
+            element.html(slide.content);
+            
+            // create AShowSlide instance for element.
+            var instance = AShowSlide.MakeSlideWithElementForShow(element, show, {
+               'index' : slide.index
+            });
+            
+            // Check if this is last slide to load.
+            if (scope.$last) {
+               // Display show navigation controls.
+               show.createControls();
+               // Call show's advance method to display first slide.
+               show.advance();
+            }
+         }
+         else
+         {   
+            if (!slide) throw new Error("aSlide directive ERROR: slide object not found in scope.");
+            if (!show) throw new Error("aSlide directive ERROR: show object '"+showId+"' not found in scope.");
+         }
+      }
+   };
 });
 
 //
 // Services
 //
 angular.module("AShow.services", [
-	"ngResource"
+   "ngResource"
 ])
 .factory("Slides", ["$resource", "$q",
-	function($resource, $q) {
-		
-		
-		//
-		// Lookup via api call...
-		// Comment out this section (including return) to use json file lookup instead.
-		//
-		var result = $resource('api/slides/:showId');
-		return result;
-		
-		
-		//
-		// Lookup from json file...
-		// Comment out this section (including return) to use hardcoded list instead.
-		//
-		var result = $resource('slides/:showId.json');
-		return result;
-		
-		
-		//
-		// Lookup from hardcoded value list...
-		// (Works when browsing files locally)
-		// Requires commenting out above lookups and calling
-		//		Slides.forShow(show.slideshow_id)
-		// instead of
-		//		Slides.query({showId:show.slideshow_id});
-		//
-		return {
-			forShow : (function(showId, callback) {
-				//
-				// Lookup from hard-coded value list.
-				// (In lieu of ability to make $resource call when running as local file
-				// in browser)
-				//
-				var slides_by_show = {
-					"example1" : [
-						{	
-							"index" : [0,0],
-							"content" : "Example1 Slide 1..."
-						},
-						{	
-							"index" : [1,0],
-							"content" : "Example1 Slide 2..."
-						},
-						{	
-							"index" : [2,0],
-							"content" : "Example1 Slide 3..."
-						}
-					],
-					"example2" : [
-						{	
-							"index" : [0,0],
-							"content" : "Example2 Slide 1..."
-						},
-						{	
-							"index" : [1,0],
-							"content" : "Example2 Slide 2..."
-						},
-						{	
-							"index" : [2,0],
-							"content" : "Example2 Slide 3..."
-						}
-					],
-					"example3" : [
-						{	
-							"index" : [0,0],
-							"content" : "Example3 Slide 1..."
-						},
-						{	
-							"index" : [0,1],
-							"content" : "Example3 Slide 2..."
-						},
-						{	
-							"index" : [0,2],
-							"content" : "Example3 Slide 3..."
-						}
-					],
-					"example4" : [
-						{	
-							"index" : [0,0],
-							"content" : "Example4 Slide 0,0..."
-						},
-						{	
-							"index" : [0,1],
-							"content" : "Example4 Slide 0,1..."
-						},
-						{	
-							"index" : [0,-1],
-							"content" : "Example4 Slide 0,-1..."
-						},
-						{	
-							"index" : [1,0],
-							"content" : "Example4 Slide 1,0..."
-						},
-						{	
-							"index" : [1,1],
-							"content" : "Example4 Slide 1,1..."
-						},
-						{	
-							"index" : [1,-1],
-							"content" : "Example4 Slide 1,-1..."
-						},
-						{	
-							"index" : [2,0],
-							"content" : "Example4 Slide 2,0..."
-						},
-						{	
-							"index" : [2,1],
-							"content" : "Example4 Slide 2,1..."
-						},
-						{	
-							"index" : [2,-1],
-							"content" : "Example4 Slide 2,-1..."
-						}
-					],
-					"gemHunter" : [
-						{
-							"index" : [0,0],
-							"content" : "<div style='background-image:url(images/maze_room_0_0.jpg)'><div class='dialog' onclick='dismiss(this)'><p>You have fallen into a labyrinth, and must find your way out.</p><p>You can tap items to collect them along the way.</p></div></div>"
-						},
-						{
-							"index" : [1,0],
-							"content" : "<img src='images/maze_room_1_0.jpg'/>"
-						},
-						{
-							"index" : [1,-1],
-							"content" : "<img src='images/maze_room_1_-1.jpg'/>"
-						},
-						{
-							"index" : [2,-1],
-							"content" : "<div style='background-image:url(images/maze_room_2_-1.jpg)'><img id='key' class='collectable' onclick='collect(this)' style='top:175px;left:310px'/></div>"
-						},
-						{
-							"index" : [1,1],
-							"content" : "<img src='images/maze_room_1_1.jpg'/>"
-						},
-						{
-							"index" : [2,1],
-							"content" : "<img src='images/maze_room_2_1.jpg'/>"
-						},
-						{
-							"index" : [1,2],
-							"content" : "<img src='images/maze_room_1_2.jpg'/>"
-						},
-						{
-							"index" : [0,2],
-							"content" : "<div style='background-image:url(images/maze_room_0_2.jpg)'><img id='gems' class='collectable' onclick='collect(this)' style='top:200px;left:175px'/></div>"
-						},
-						{
-							"index" : [1,3],
-							"content" : "<img src='images/maze_room_1_3.jpg'/>"
-						},
-						{
-							"index" : [2,3],
-							"content" : "<img src='images/maze_room_2_3.jpg'/>"
-						},
-						{
-							"index" : [3,3],
-							"content" : "<img src='images/maze_room_3_3.jpg'/>"
-						},
-						{
-							"index" : [3,2],
-							"content" : "<img src='images/maze_room_3_2.jpg'/>"
-						},
-						{
-							"index" : [3,0],
-							"content" : "<div id='goal'><h1>Congratulations!</h1><p>You have escaped the maze.</p><div id='score'></div></div>"
-						}
-					]
-				};
-				//
-				// Return promise to simulate resource call.
-				//
-				var getSlidesForShow = (function(showId) {
-					var deferred = $q.defer(),
-						promise = deferred.promise,
-						data = slides_by_show[showId];
-					if (data) {
-						deferred.resolve(data);
-					} else {
-						deferred.reject("No slides found for show id '"+showId+"'.");
-					}
-					
-					promise.then(function(value) {
-						// deferred.resolve callback
-						// Apply vales to promise object to approximate resource behavior.
-						for (var i in value) promise[i] = value[i];
-						promise.length = value.length;
-						promise.$resolved = true;
-						if (callback) callback();
-					}, function(reason) {
-						// deferred.reject callback
-						throw new Error(reason);
-					}, function(update) {
-						// deferred.notify callback
-						console.log(update);
-					});
-					
-					return promise;
-				});
-				var result = getSlidesForShow(showId);
-				return result;
-			})
-		};
-		
-	}
+   function($resource, $q) {
+      
+      
+      //
+      // Lookup via api call...
+      // Comment out this section (including return) to use json file lookup instead.
+      //
+      var result = $resource('api/slides/:showId');
+      return result;
+      
+      
+      //
+      // Lookup from json file...
+      // Comment out this section (including return) to use hardcoded list instead.
+      //
+      var result = $resource('slides/:showId.json');
+      return result;
+      
+      
+      //
+      // Lookup from hardcoded value list...
+      // (Works when browsing files locally)
+      // Requires commenting out above lookups and calling
+      //      Slides.forShow(show.slideshow_id)
+      // instead of
+      //      Slides.query({showId:show.slideshow_id});
+      //
+      return {
+         forShow : function(showId, callback) {
+            //
+            // Lookup from hard-coded value list.
+            // (In lieu of ability to make $resource call when running as local file
+            // in browser)
+            //
+            var slides_by_show = {
+               "example1" : [
+                  {   
+                     "index" : [0,0],
+                     "content" : "Example1 Slide 1..."
+                  },
+                  {   
+                     "index" : [1,0],
+                     "content" : "Example1 Slide 2..."
+                  },
+                  {   
+                     "index" : [2,0],
+                     "content" : "Example1 Slide 3..."
+                  }
+               ],
+               "example2" : [
+                  {   
+                     "index" : [0,0],
+                     "content" : "Example2 Slide 1..."
+                  },
+                  {   
+                     "index" : [1,0],
+                     "content" : "Example2 Slide 2..."
+                  },
+                  {   
+                     "index" : [2,0],
+                     "content" : "Example2 Slide 3..."
+                  }
+               ],
+               "example3" : [
+                  {   
+                     "index" : [0,0],
+                     "content" : "Example3 Slide 1..."
+                  },
+                  {   
+                     "index" : [0,1],
+                     "content" : "Example3 Slide 2..."
+                  },
+                  {   
+                     "index" : [0,2],
+                     "content" : "Example3 Slide 3..."
+                  }
+               ],
+               "example4" : [
+                  {   
+                     "index" : [0,0],
+                     "content" : "Example4 Slide 0,0..."
+                  },
+                  {   
+                     "index" : [0,1],
+                     "content" : "Example4 Slide 0,1..."
+                  },
+                  {   
+                     "index" : [0,-1],
+                     "content" : "Example4 Slide 0,-1..."
+                  },
+                  {   
+                     "index" : [1,0],
+                     "content" : "Example4 Slide 1,0..."
+                  },
+                  {   
+                     "index" : [1,1],
+                     "content" : "Example4 Slide 1,1..."
+                  },
+                  {   
+                     "index" : [1,-1],
+                     "content" : "Example4 Slide 1,-1..."
+                  },
+                  {   
+                     "index" : [2,0],
+                     "content" : "Example4 Slide 2,0..."
+                  },
+                  {   
+                     "index" : [2,1],
+                     "content" : "Example4 Slide 2,1..."
+                  },
+                  {   
+                     "index" : [2,-1],
+                     "content" : "Example4 Slide 2,-1..."
+                  }
+               ],
+               "gemHunter" : [
+                  {
+                     "index" : [0,0],
+                     "content" : "<div style='background-image:url(images/maze_room_0_0.jpg)'><div class='dialog' onclick='dismiss(this)'><p>You have fallen into a labyrinth, and must find your way out.</p><p>You can tap items to collect them along the way.</p></div></div>"
+                  },
+                  {
+                     "index" : [1,0],
+                     "content" : "<img src='images/maze_room_1_0.jpg'/>"
+                  },
+                  {
+                     "index" : [1,-1],
+                     "content" : "<img src='images/maze_room_1_-1.jpg'/>"
+                  },
+                  {
+                     "index" : [2,-1],
+                     "content" : "<div style='background-image:url(images/maze_room_2_-1.jpg)'><img id='key' class='collectable' onclick='collect(this)' style='top:175px;left:310px'/></div>"
+                  },
+                  {
+                     "index" : [1,1],
+                     "content" : "<img src='images/maze_room_1_1.jpg'/>"
+                  },
+                  {
+                     "index" : [2,1],
+                     "content" : "<img src='images/maze_room_2_1.jpg'/>"
+                  },
+                  {
+                     "index" : [1,2],
+                     "content" : "<img src='images/maze_room_1_2.jpg'/>"
+                  },
+                  {
+                     "index" : [0,2],
+                     "content" : "<div style='background-image:url(images/maze_room_0_2.jpg)'><img id='gems' class='collectable' onclick='collect(this)' style='top:200px;left:175px'/></div>"
+                  },
+                  {
+                     "index" : [1,3],
+                     "content" : "<img src='images/maze_room_1_3.jpg'/>"
+                  },
+                  {
+                     "index" : [2,3],
+                     "content" : "<img src='images/maze_room_2_3.jpg'/>"
+                  },
+                  {
+                     "index" : [3,3],
+                     "content" : "<img src='images/maze_room_3_3.jpg'/>"
+                  },
+                  {
+                     "index" : [3,2],
+                     "content" : "<img src='images/maze_room_3_2.jpg'/>"
+                  },
+                  {
+                     "index" : [3,0],
+                     "content" : "<div id='goal'><h1>Congratulations!</h1><p>You have escaped the maze.</p><div id='score'></div></div>"
+                  }
+               ]
+            };
+            //
+            // Return promise to simulate resource call.
+            //
+            var getSlidesForShow = function(showId) {
+               var deferred = $q.defer(),
+                  promise = deferred.promise,
+                  data = slides_by_show[showId];
+               if (data) {
+                  deferred.resolve(data);
+               } else {
+                  deferred.reject("No slides found for show id '"+showId+"'.");
+               }
+               
+               promise.then(function(value) {
+                  // deferred.resolve callback
+                  // Apply vales to promise object to approximate resource behavior.
+                  for (var i in value) promise[i] = value[i];
+                  promise.length = value.length;
+                  promise.$resolved = true;
+                  if (callback) callback();
+               }, function(reason) {
+                  // deferred.reject callback
+                  throw new Error(reason);
+               }, function(update) {
+                  // deferred.notify callback
+                  console.log(update);
+               });
+               
+               return promise;
+            };
+            var result = getSlidesForShow(showId);
+            return result;
+         }
+      };
+      
+   }
 ]);
--- public/AShowAngularSpec.js
+++ public/AShowAngularSpec.js
@@ -1,101 +1,101 @@
 
 describe("AShowAngular", function() {
-	describe("modules should have no existential crises", function() {
-		//
-		// basic module existence tests...
-		//
-		it("should have defined 'AShow' module", function() {
-			expect(angular.module('AShow')).toBeDefined();
-		});
-		it("should have defined 'AShow.services' module", function() {
-			expect(angular.module('AShow.services')).toBeDefined();
-		});
-		it("should have defined 'AShow.directives' module", function() {
-			expect(angular.module('AShow.directives')).toBeDefined();
-		});
-	});
-	describe("injectables", function() {
-		var injector;
-		beforeEach(function() {
-			injector = angular.element('body').injector();
-		});
-		it("injector should be able to invoke self", function() {
-			expect(injector.get('$injector')).toBe(injector);
-			expect(injector.invoke(function($injector) {
-				return $injector;
-			})).toBe(injector);
-		});
-		it("injector should be able to inject $compile and $rootScope", function() {
-			injector.invoke(['$compile','$rootScope', function($compile, $rootScope) {
-				expect($compile).toBeTruthy();
-				expect($rootScope).toBeTruthy();
-			}]);
-		});
-		
-		it("should be able to retrieve slides via service", function() {
-			injector.invoke(['Slides', function(Slides) {
-				var slides = Slides.query({showId:'example1'}); // json or api
-				//var slides = Slides.forShow('example1'); // if browsing local files
-				
-				waitsFor(function() {
-					return slides.length > 0;
-				}, "should have finished loading slides",
-				2000);
-				
-				runs(function() {
-					expect(slides.length).toBeTruthy();
-					expect(slides[0].index).toBeTruthy();
-					expect(slides[0].content).toBeTruthy();
-				});
-			}]);
-		});
-		
-		it("should be able to process aSlideshow directive", function() {
-			injector.invoke(['$compile','$rootScope', function($compile, $rootScope) {
-				var testShowElement = $compile(
-					"<div id='test' a-slideshow></div>"
-					)($rootScope);
-				$rootScope.$digest();
-				
-				// Directive should have created AShow instance linked to element.
-				var show = testShowElement[0].AShow;
-				expect(show).toBeDefined();
-				// Make sure property is created from attribute.
-				expect(show.slideshow_id).toEqual('test');
-			}]);
-		});
-		it("should be able to process aSlide directive", function() {
-			injector.invoke(['$compile','$rootScope', function($compile, $rootScope) {
-				// slideshow_id of example1 should find slides, thus triggering creation
-				// of slide elements bearing the a-slide directive.
-				var testShowElement = $compile(
-					"<div id='example1' a-slideshow></div>"
-					)($rootScope);
-				$rootScope.$digest();
-				
-				// Directive should have created a .slide elements
-				expect(testShowElement.find('.AShowSlide').length).toBeTruthy();
-				
-				// Directive should populate slides for show.
-				var show = testShowElement[0].AShow;
-				expect(show).toBeTruthy();
-				
-				waitsFor(function() {
-					return show.slides.length > 0;
-				}, "slides to finished loading",
-				5000);
-				
-				runs(function() {
-					expect(show.slidesByIndex).toBeTruthy();
-					expect(show.slidesByIndex['0,0']).toBeTruthy();
-					expect(show.slidesByIndex['0,0'].element).toBeTruthy();
-					expect(show.slidesByIndex['0,0'].element.html()).toEqual(
-						"First example slide..."
-					);
-				});
-				
-			}]);
-		});
-	});
-	
+   describe("modules should have no existential crises", function() {
+      //
+      // basic module existence tests...
+      //
+      it("should have defined 'AShow' module", function() {
+         expect(angular.module('AShow')).toBeDefined();
+      });
+      it("should have defined 'AShow.services' module", function() {
+         expect(angular.module('AShow.services')).toBeDefined();
+      });
+      it("should have defined 'AShow.directives' module", function() {
+         expect(angular.module('AShow.directives')).toBeDefined();
+      });
+   });
+   describe("injectables", function() {
+      var injector;
+      beforeEach(function() {
+         injector = angular.element('body').injector();
+      });
+      it("injector should be able to invoke self", function() {
+         expect(injector.get('$injector')).toBe(injector);
+         expect(injector.invoke(function($injector) {
+            return $injector;
+         })).toBe(injector);
+      });
+      it("injector should be able to inject $compile and $rootScope", function() {
+         injector.invoke(['$compile','$rootScope', function($compile, $rootScope) {
+            expect($compile).toBeTruthy();
+            expect($rootScope).toBeTruthy();
+         }]);
+      });
+      
+      it("should be able to retrieve slides via service", function() {
+         injector.invoke(['Slides', function(Slides) {
+            var slides = Slides.query({showId:'example1'}); // json or api
+            //var slides = Slides.forShow('example1'); // if browsing local files
+            
+            waitsFor(function() {
+               return slides.length > 0;
+            }, "should have finished loading slides",
+            2000);
+            
+            runs(function() {
+               expect(slides.length).toBeTruthy();
+               expect(slides[0].index).toBeTruthy();
+               expect(slides[0].content).toBeTruthy();
+            });
+         }]);
+      });
+      
+      it("should be able to process aSlideshow directive", function() {
+         injector.invoke(['$compile','$rootScope', function($compile, $rootScope) {
+            var testShowElement = $compile(
+               "<div id='test' a-slideshow></div>"
+               )($rootScope);
+            $rootScope.$digest();
+            
+            // Directive should have created AShow instance linked to element.
+            var show = testShowElement[0].AShow;
+            expect(show).toBeDefined();
+            // Make sure property is created from attribute.
+            expect(show.slideshow_id).toEqual('test');
+         }]);
+      });
+      it("should be able to process aSlide directive", function() {
+         injector.invoke(['$compile','$rootScope', function($compile, $rootScope) {
+            // slideshow_id of example1 should find slides, thus triggering creation
+            // of slide elements bearing the a-slide directive.
+            var testShowElement = $compile(
+               "<div id='example1' a-slideshow></div>"
+               )($rootScope);
+            $rootScope.$digest();
+            
+            // Directive should have created a .slide elements
+            expect(testShowElement.find('.AShowSlide').length).toBeTruthy();
+            
+            // Directive should populate slides for show.
+            var show = testShowElement[0].AShow;
+            expect(show).toBeTruthy();
+            
+            waitsFor(function() {
+               return show.slides.length > 0;
+            }, "slides to finished loading",
+            5000);
+            
+            runs(function() {
+               expect(show.slidesByIndex).toBeTruthy();
+               expect(show.slidesByIndex['0,0']).toBeTruthy();
+               expect(show.slidesByIndex['0,0'].element).toBeTruthy();
+               expect(show.slidesByIndex['0,0'].element.html()).toEqual(
+                  "First example slide..."
+               );
+            });
+            
+         }]);
+      });
+   });
+   
 });
